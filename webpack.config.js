1b93ba4c75c1a3aca21c5aa54626a6f04830646f
Dinorah Perez Hincapie <dperezhincapie@gmail.com>
2020-06-11 01:47:38 -0500

    Change port on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     watch: false,
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        port: 3000,
+        port: 5000,
         compress: true,
         open: true,
         historyApiFallback: true
